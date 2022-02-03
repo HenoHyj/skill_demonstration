6a630e3192f608f508c064698b58fa608f698532
HYJB22 <2681095614@qq.com>
2022-02-02 20:40:51 -0800

    Change in code to make it correct

--- TestSkillDemo.java
+++ TestSkillDemo.java
@@ -4,6 +4,13 @@ import org.junit.*;
 public class TestSkillDemo{
     @Test
     public void testMinus() {
-        assertEquals(2, skillDemo.minus(5,1));
+        assertEquals(2, skillDemo.minus(5,3));
     }
 }
+
+
+/**
+ * Linux:
+ * javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar TestSkillDemo.java
+ * java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestSkillDemo
+ */
